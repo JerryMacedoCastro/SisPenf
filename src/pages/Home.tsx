753b23aead36a910aa978577615a53e314baf2f0
Jerry Macedo Castro <leom551@hotmail.com>
2021-02-07 20:04:00 -0300

    button component and panel tab incomplete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-
-import Gradient from '../components/Gradient';
+// import { StyleSheet } from 'react-native';
 
 import TabsNavigation from './TabsNavigation';
-import Header from '../components/Header';
 
 const Home = () => {
   return (
@@ -17,11 +13,11 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#BCE0DC',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#BCE0DC',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- src/pages/PanelTab.tsx
+++ src/pages/PanelTab.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+
 import Gradient from '../components/Gradient';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 const PanelTab = () => {
+  const data = [
+    {
+      name: 'Seoul',
+      population: 21500000,
+      color: 'rgba(131, 167, 234, 1)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Toronto',
+      population: 2800000,
+      color: '#F00',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+  ];
   return (
     <View style={styles.container}>
       <Gradient />
       <Header title="Olá, jessica" />
-      <Text>Painel geral</Text>
+      <View style={[styles.content, styles.info]}></View>
+      <View style={styles.content}>
+        <Button title="Admitir paciente" icon="log-in" color="#FFF" size={24} />
+        <Button
+          title="Processo de Enfermagem"
+          icon="user-check"
+          color="#FFF"
+          size={24}
+        />
+        <Button
+          title="Pendências"
+          icon="alert-triangle"
+          color="#FFF"
+          size={24}
+        />
+        <Button
+          title="Acompanhar paciente"
+          icon="file-text"
+          color="#FFF"
+          size={24}
+        />
+        <Button
+          title="Intercorrências"
+          icon="file-plus"
+          color="#FFF"
+          size={24}
+        />
+        <Button title="Alta médica" icon="log-out" color="#FFF" size={24} />
+      </View>
     </View>
   );
 };
@@ -21,5 +67,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#BCE0DC',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  content: {
+    backgroundColor: '#fff',
+    width: '90%',
+    padding: 8,
+    position: 'relative',
+    borderRadius: 25,
+  },
+
+  info: {
+    height: 100,
+    marginBottom: 25,
+    borderWidth: 1,
+    borderColor: '#27615A',
   },
 });
