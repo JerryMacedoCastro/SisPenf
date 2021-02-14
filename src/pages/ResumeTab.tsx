4c8255554a209c8f25696b6e7ae5ac5189d8b740
Jerry Macedo Castro <leom551@hotmail.com>
2021-02-13 21:31:46 -0300

    Horizontal scrow view at resumeTab page

--- src/pages/ResumeTab.tsx
+++ src/pages/ResumeTab.tsx
@@ -5,18 +5,16 @@ import Fieldset from '../components/Fieldset';
 
 import Gradient from '../components/Gradient';
 import Header from '../components/Header';
+import PatientsList from '../components/PatientsList';
+import Scrollview from '../components/Scrollview';
 
 export default function ResumeTab() {
   return (
     <View style={styles.container}>
       <Gradient />
       <Header title="Olá, Jessica" />
-      <View style={styles.content}>
-        <Fieldset label="Leito 01" value="Maria Ribeiro da Costa e Lima" />
-        <Fieldset label="Leito 02" value="Maria Ribeiro da Costa e Lima" />
-        <Fieldset label="Leito 03" value="Maria Ribeiro da Costa e Lima" />
-        <Fieldset label="Leito 04" value="Maria Ribeiro da Costa e Lima" />
-      </View>
+      <Scrollview />
+      <PatientsList />
     </View>
   );
 }
@@ -27,16 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#BCE0DC',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-
-  content: {
-    backgroundColor: '#fff',
-    width: '90%',
-    padding: 8,
-    position: 'relative',
-    borderRadius: 25,
-    display: 'flex',
-    alignItems: 'center',
-    height: '60%',
   },
 });
