ec7a067e54ec2066bdfbec2f1870613a84f7f794
Jerry Macedo Castro <leom551@hotmail.com>
2021-02-15 20:20:21 -0300

    commom input component

---
+++ src/components/CommonInput.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+const CommonInput = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.inputText} placeholder="Nome"></TextInput>
+    </View>
+  );
+};
+
+export default CommonInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    borderBottomWidth: 3,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderRadius: 20,
+    borderColor: 'rgba(196, 196, 196, 0.7)',
+    borderTopWidth: 0,
+    elevation: 0,
+    padding: 4,
+    margin: 10,
+  },
+  inputText: {
+    padding: 6,
+    color: '#34615C',
+  },
+});
--- src/pages/NewPuerperal.tsx
+++ src/pages/NewPuerperal.tsx
@@ -1,6 +1,10 @@
+import { Feather } from '@expo/vector-icons';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Button from '../components/Button';
+import { RectButton } from 'react-native-gesture-handler';
+import { globalStyles } from '../Assets/GlobalStyles';
+import CommonInput from '../components/CommonInput';
+
 import DateHeader from '../components/DateHeader';
 
 const NewPuerperal = () => {
@@ -8,8 +12,26 @@ const NewPuerperal = () => {
     <>
       <View style={styles.container}>
         <DateHeader />
-
-        <View style={styles.content}></View>
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.button, globalStyles.primaryButton]}>
+            <Text style={globalStyles.primaryButtonText}>
+              Selecionar enfermaria
+            </Text>
+            <Feather name="chevrons-down" color="#fff" size={18} />
+          </RectButton>
+          <RectButton style={[styles.button, globalStyles.primaryButton]}>
+            <Text style={globalStyles.primaryButtonText}>Selecionar leito</Text>
+            <Feather name="chevrons-down" color="#fff" size={18} />
+          </RectButton>
+        </View>
+        <View style={styles.content}>
+          <CommonInput />
+          <CommonInput />
+          <CommonInput />
+          <CommonInput />
+          <CommonInput />
+          <CommonInput />
+        </View>
       </View>
     </>
   );
@@ -23,11 +45,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#BCE0DC',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
+  button: {
+    borderRadius: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 42,
+    marginTop: 10,
+    width: '45%',
+    fontFamily: 'JosefinSans_700Bold',
+    padding: 16,
+  },
+
   content: {
-    backgroundColor: 'rgba(196, 196, 196, 0.7)',
+    backgroundColor: 'rgb(220, 220, 220)',
     height: '60%',
     width: '90%',
     borderRadius: 20,
+  },
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    padding: 16,
   },
 });
