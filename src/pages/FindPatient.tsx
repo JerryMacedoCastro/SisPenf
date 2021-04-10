04a50a020d3533bf77852c3c93a37f222c83210b
Jerry Macedo Castro <leom551@hotmail.com>
2021-04-10 11:53:30 -0300

    create input for search patients

--- src/pages/FindPatient.tsx
+++ src/pages/FindPatient.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import React, { useLayoutEffect, useRef } from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
 import DateHeader from '../components/DateHeader'
 import Gradient from '../components/Gradient'
 
 const FindPatient = () => {
+  //<TextInput> or others dont work here
+  const searchInput = useRef<any>(null)
+  const handleSearchPress = () => {
+    if (searchInput && searchInput.current) {
+      searchInput.current.focus();
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Gradient />
       <DateHeader title="Buscar Paciente" />
-      <Text>Find Patient</Text>
+      <Text style={styles.label}>Buscar paciente pelo nome</Text>
+      <View style={styles.inputContainer}>
+        <TextInput ref={searchInput} placeholder={"Digite o nome do paciente"} style={styles.input} />
+        <Feather name={"search"} color={"#34615C"} size={24} onPress={handleSearchPress} />
+      </View>
+
     </View>
   )
 }
@@ -22,4 +37,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    color: "#34615C",
+    alignSelf: 'flex-start',
+    marginLeft: '10%',
+    marginBottom: 4,
+    fontSize: 16,
+    fontFamily: 'JosefinSans_700Bold',
+  },
+  inputContainer: {
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: "space-around",
+    width: '90%',
+    backgroundColor: '#fff',
+    paddingHorizontal: 1,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#34615C',
+  },
+  input: {
+    width: '80%',
+    color: '#34615C',
+
+  }
 })
