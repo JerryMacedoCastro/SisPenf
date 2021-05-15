ea3147e0fd604379372dca4c5918ffaa697e1a3e
Jerry Macedo Castro <leom551@hotmail.com>
2021-05-15 17:54:49 -0300

    chore: create pages to physical exam

---
+++ src/pages/PhysicalExam/PartOne/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, Text, Platform, KeyboardAvoidingView } from "react-native";
+
+import { styles } from "../styles";
+
+import DateHeader from "../../../components/DateHeader";
+import SelectableButton from "../../../components/SelectableButton";
+import Gradient from "../../../components/Gradient";
+import { RectButton, ScrollView } from "react-native-gesture-handler";
+import { globalStyles } from "../../../Assets/GlobalStyles";
+import useKeyboardControll from "../../../hooks/useKeyboardControll";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+const index = (): JSX.Element => {
+  const navigation = useNavigation();
+  const isKeyboardShown = useKeyboardControll();
+  const handleContinue = () => {
+    navigation.navigate("PartTwo");
+  };
+
+  return (
+    <>
+      {!isKeyboardShown && (
+        <DateHeader
+          title="Exame físico parte 1"
+          destinyBack=" ChildbirthData"
+        />
+      )}
+      <SafeAreaView style={styles.container}>
+        <Gradient />
+        <KeyboardAvoidingView
+          style={styles.content}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={50}
+        >
+          <ScrollView
+            contentContainerStyle={{
+              position: "relative",
+            }}
+          >
+            <View style={styles.formContainer}>
+              <SelectableButton title="Admitir paciente" />
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+
+        {!isKeyboardShown && (
+          <View style={styles.confirmButtonsContainer}>
+            <RectButton
+              style={[globalStyles.button, globalStyles.primaryButton]}
+              onPress={handleContinue}
+            >
+              <Text style={globalStyles.primaryButtonText}>Continuar</Text>
+            </RectButton>
+          </View>
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default index;
---
+++ src/pages/PhysicalExam/PartTwo/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, Text, Platform, KeyboardAvoidingView } from "react-native";
+
+import { styles } from "../styles";
+
+import DateHeader from "../../../components/DateHeader";
+import Gradient from "../../../components/Gradient";
+import { RectButton, ScrollView } from "react-native-gesture-handler";
+import { globalStyles } from "../../../Assets/GlobalStyles";
+import useKeyboardControll from "../../../hooks/useKeyboardControll";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+const index = (): JSX.Element => {
+  const navigation = useNavigation();
+  const isKeyboardShown = useKeyboardControll();
+  const handleContinue = () => {
+    navigation.navigate("");
+  };
+
+  return (
+    <>
+      {!isKeyboardShown && (
+        <DateHeader title="Exame físico parte 2" destinyBack="PartOne" />
+      )}
+      <SafeAreaView style={styles.container}>
+        <Gradient />
+        <KeyboardAvoidingView
+          style={styles.content}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={50}
+        >
+          <ScrollView
+            contentContainerStyle={{
+              position: "relative",
+            }}
+          >
+            <View style={styles.formContainer}></View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+
+        {!isKeyboardShown && (
+          <View style={styles.confirmButtonsContainer}>
+            <RectButton
+              style={[globalStyles.button, globalStyles.primaryButton]}
+              onPress={handleContinue}
+            >
+              <Text style={globalStyles.primaryButtonText}>Continuar</Text>
+            </RectButton>
+          </View>
+        )}
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default index;
